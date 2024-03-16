793b2ba8e1f714732fb1db418ea342a534c62240
Nionim <126122210+Nionim@users.noreply.github.com>
2024-03-16 22:26:54 +0300

    Update LoginEvent.java

--- src/main/java/delta/cion/WhiteList/LoginEvent.java
+++ src/main/java/delta/cion/WhiteList/LoginEvent.java
@@ -17,15 +17,18 @@ public class LoginEvent implements Listener {
 
     @EventHandler
     public void PlayerJoin(PlayerLoginEvent event) {
-        if (Violet_WhiteList.getInstance().getConfig().getBoolean("enable_disable")) {
+        log("h", "Зареган евент");
+        if (Objects.requireNonNull(Violet_WhiteList.getInstance().getConfig().getString("enable_disable")).equalsIgnoreCase("enable")) {
+            log("h", "Зареган статус");
             Player player = event.getPlayer();
             List<String> whitelisted = Violet_WhiteList.getInstance().getConfig().getConfigurationSection("Whitelist").getStringList("players").stream().map(String::toLowerCase).collect(Collectors.toList());
             String pname = player.getName();
             String reason = Objects.requireNonNull(Violet_WhiteList.getInstance().getConfig().getString("no_WL")).replace("{player}", pname);
             String prefix = Violet_WhiteList.getInstance().getConfig().getString("prefix");
             if (!whitelisted.contains(pname.toLowerCase())) {
+                log("h", "кикнул");
                 event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', prefix + reason));
-            }
+            } else {log("h", "не кикнул");}
         }
     }
 }
