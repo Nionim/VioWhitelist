2904b6b032e4abb495f67d09cf4c351bfa04132d
Nionim <126122210+Nionim@users.noreply.github.com>
2024-03-23 23:42:14 +0300

    Update LoginEvent.java

--- src/main/java/delta/cion/WhiteList/LoginEvent.java
+++ src/main/java/delta/cion/WhiteList/LoginEvent.java
@@ -1,6 +1,6 @@
 package delta.cion.WhiteList;
 
-import delta.cion.Violet_WhiteList;
+import delta.cion.Utils.Senders;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,20 +11,26 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static delta.cion.Utils.Senders.log;
+import static delta.cion.WhiteList.WLUtils.config;
 
 public class LoginEvent implements Listener {
 
     @EventHandler
     public void PlayerJoin(PlayerLoginEvent event) {
-        if (Objects.requireNonNull(Violet_WhiteList.getInstance().getConfig().getString("enable_disable")).equalsIgnoreCase("enable")) {
-            Player player = event.getPlayer();
-            List<String> whitelisted = Violet_WhiteList.getInstance().getConfig().getConfigurationSection("Whitelist").getStringList("players").stream().map(String::toLowerCase).collect(Collectors.toList());
-            String pname = player.getName();
-            String reason = Objects.requireNonNull(Violet_WhiteList.getInstance().getConfig().getString("no_WL")).replace("{player}", pname);
-            String prefix = Violet_WhiteList.getInstance().getConfig().getString("prefix");
-            if (!whitelisted.contains(pname.toLowerCase())) {
-                event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', prefix + reason));
+
+        if (Objects.requireNonNull(config.getString("enable_disable")).equalsIgnoreCase("enable")) {
+            try {
+                Player player = event.getPlayer();
+                String p_name = player.getName().toLowerCase();
+                String prefix = config.getString("prefix");
+                String reason = Objects.requireNonNull(config.getString("no_WL")).replace("{player}", p_name);
+                List<String> whitelisted = config.getConfigurationSection("Whitelist").getStringList("players").stream().map(String::toLowerCase).collect(Collectors.toList());
+                if (!whitelisted.isEmpty() && !whitelisted.contains(p_name)) {
+                        event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', prefix + reason));
+                }
+            } catch (NullPointerException e) {
+                Senders.log("l", "&4Config error:&c\n"+e);
+            }
         }
     }
 }
