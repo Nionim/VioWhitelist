3a70b2adb1100219edddd7c6ee9808738cd0b0a5
Nionim <126122210+Nionim@users.noreply.github.com>
2024-03-23 23:42:00 +0300

    Update WLUtils.java

--- src/main/java/delta/cion/WhiteList/WLUtils.java
+++ src/main/java/delta/cion/WhiteList/WLUtils.java
@@ -2,6 +2,9 @@ package delta.cion.WhiteList;
 
 import delta.cion.Violet_WhiteList;
 import lombok.Getter;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -9,13 +12,18 @@ import java.util.Objects;
 public class WLUtils {
 
     @Getter
-    private static WLUtils instance = new WLUtils();
+    public static WLUtils instance = new WLUtils();
     private final List<String> whitelisted;
+    public static FileConfiguration config;
+    public static ConfigurationSection MSG;
 
-
-    private WLUtils() {
+    public WLUtils() {
         instance = this;
+        // Conf
+        config = Violet_WhiteList.getInstance().getConfig();
+        MSG = config.getConfigurationSection("Messages");
         // List
-        whitelisted = Objects.requireNonNull(Violet_WhiteList.getInstance().getConfig().getConfigurationSection("Whitelist")).getStringList("players");
+        whitelisted = Objects.requireNonNull(config.getConfigurationSection("Whitelist")).getStringList("players");
+
     }
 }
