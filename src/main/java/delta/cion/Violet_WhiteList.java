ac0417d561dc45f129034db8bab0f4540c6d9c70
Nionim <126122210+Nionim@users.noreply.github.com>
2024-03-23 23:42:29 +0300

    Update Violet_WhiteList.java

--- src/main/java/delta/cion/Violet_WhiteList.java
+++ src/main/java/delta/cion/Violet_WhiteList.java
@@ -17,25 +17,18 @@ public final class Violet_WhiteList extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        saveDefaultConfig();
 
-        String startMSG =
-                "&5By&6: &2Carde2"+
-                "\n"+
-                "&5For&6: &2Violett Project"+
-                "\n"+
-                "&5DS&6: &2https://discord.gg/MEBkvJbe4P";
+        this.saveDefaultConfig();
+        Objects.requireNonNull(this.getCommand("viowl")).setExecutor(new MainCommand());
+        Objects.requireNonNull(this.getCommand("viowl")).setTabCompleter(new MainCommand());
+        this.getServer().getPluginManager().registerEvents(new LoginEvent(), this);
 
+        String startMSG = "&5By&6: &2Carde2"+"\n"+"&5For&6: &2Violett Project"+"\n"+"&5DS&6: &2https://discord.gg/MEBkvJbe4P";
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', startMSG));
-
-        Objects.requireNonNull(getCommand("viowl")).setExecutor(new MainCommand());
-        Objects.requireNonNull(getCommand("viowl")).setTabCompleter(new MainCommand());
-        getServer().getPluginManager().registerEvents(new LoginEvent(), this);
-
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&6Удачи!"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&5DEVELOPER&8] &7Carde2&6: &dУдачи&6!"));
     }
 }
